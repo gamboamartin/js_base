208984f3d14ed962c043ca7d9bcd1cf03b87f733
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-08 16:25:51 -0500

    v0.4.0 Se integra get absolute path de java en base

--- src/base.js
+++ src/base.js
@@ -1,3 +1,8 @@
+function getAbsolutePath() {
+    var loc = window.location;
+    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     const regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
