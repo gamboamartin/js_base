e5f9fa465b1b5221c320a8f64822d27557ea7997
javier <javier.flores@ivitec.com.mx>
2022-08-17 17:04:57 -0500

    v0.5.1 FIX menu_barras, ya no afecta a 3eros

--- src/menu_barras.js
+++ src/menu_barras.js
@@ -1,7 +1,4 @@
 $( document ).ready(function() {
-    $(".info").fadeTo(7500, 500).slideUp(500, function(){
-        $(".info").slideUp(500);
-    });
     let titulo0 = "Empresas"
     let titulo1 = "Lista"
     let titulo2 = "Empresas"
