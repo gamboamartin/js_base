a90b07c865c5dfbb00681d6e7ff43c6e5d3a619d
Kevin <kevin.acuna@ivitec.com.mx>
2022-10-27 17:29:11 -0500

    v0.7.1 Se austa new_option_sl

--- src/base.js
+++ src/base.js
@@ -9,12 +9,17 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-function integra_new_option(container, descripcion, value){
-    let new_option =new_option_sl(descripcion,value);
+function integra_new_option(container, descripcion, value,data= "",data_value = " "){
+    let new_option =new_option_sl(descripcion,value,data,data_value);
     $(new_option).appendTo(container);
 }
 
 
-function new_option_sl(descripcion,value){
-    return "<option value =" + value + ">" + descripcion + "</option>";
+function new_option_sl(descripcion,value,data= "",data_value = " "){
+
+    if (data !== ""){
+        data_value = (data_value !== "")? "="+data_value:"";
+        return `<option value ="${value}" ${data}${data_value}>${descripcion}</option>`;
+    }
+    return `<option value ="${value}">${descripcion}</option>`;
 }
