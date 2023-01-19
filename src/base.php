8d272b2c64e9b66c8b5f253be9b871c70cfb9ea0
mgamboa <gamboa.martin@gmail.com>
2023-01-19 09:26:18 -0600

    v2.1.0 Se genera funcion URL

--- src/base.php
+++ src/base.php
@@ -1,7 +1,13 @@
 <?php
 namespace gamboamartin\js_base;
 
+use config\generales;
+
 class base{
+    /**
+     * Genera una funcion de tipo java para obtener la url base de ejecucion
+     * @return string
+     */
     final public function get_absolute_path(): string
     {
         $js = "<script>";
@@ -10,6 +16,15 @@ class base{
         $js.= "var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);";
         $js .= "loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));";
         $js .= "}";
+        $js .= "</script>";
+        return $js;
+    }
+
+    final public function url_base(): string
+    {
+        $url = (new generales())->url_base;
+        $js = "<script>";
+        $js .= "cons URL = $url;";
         $js .= "</script>";
         return $js;
     }
